b0068828e0e8da8e4a4540fbb34d37eb9cb95d71
IpoLa <akramchat@hotmail.com>
2021-10-29 23:38:59 +0100

    Footer

--- Downloads/echihab-website/src/Footer.js
+++ Downloads/echihab-website/src/Footer.js
@@ -10,10 +10,10 @@ Heading,
 import {Link} from 'react-router-dom'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import CustomInput from '@mui/material/Input';
-import Button from '@mui/material/Button';
+// import YouTubeIcon from '@mui/icons-material/YouTube';
+// import TwitterIcon from '@mui/icons-material/Twitter';
+// import CustomInput from '@mui/material/Input';
+// import Button from '@mui/material/Button';
 
 const Footer = () => {
 return (
@@ -21,7 +21,9 @@ return (
 	<h1 style={{ color: "white",
 				textAlign: "center",
                 direction: "rtl",
-				marginTop: "-50px" }}>
+				marginTop: "-40px",
+				fontSize: "30px"
+				}}>
 		معلومات عنا
 	</h1>
 	<Container style={{ direction: "rtl", marginTop: "50px" }} >
@@ -29,29 +31,29 @@ return (
 		<Column style={{ direction: "rtl", marginRight: "-100px", width: "200px" }} >
 			<Heading>
                 <img 
-					style={{ height: "50px" }}
+					style={{ height: "60px" }}
                     src={process.env.PUBLIC_URL + `/logo-echihab-gold.png`}
                     alt=''
                 />
             </Heading>
-			<Link style={{ textDecoration: "none", color: "white" }} dir="rtl" to="/">الصفحة الرئيسية</Link>
+			<Link style={{ textDecoration: "none", color: "white", marginTop: "-20px" }} dir="rtl" to="/">الصفحة الرئيسية</Link>
 			<Link  style={{ textDecoration: "none", color: "white" }} dir="rtl" to="/numbers/">أعداد المجلة</Link>
 			<Link style={{ textDecoration: "none", color: "white" }} to="/team/" rel="noopener noreferrer">فريق المجلة</Link>
             <Link style={{ textDecoration: "none", color: "white" }} to={{ pathname: "https://www.paypal.com/donate?token=-_DbW3TGtjrET5eTRpFdbz-lPvjDPerlg_Kc9aA4AtpVy52J0xxO6Btf08foUqsot5yqJz-4zVfQi0de" }} target="_blank" >تبرع للمجلة</Link>
             {/* <FooterLink href="/contact">إتصل بنا</FooterLink> */}
 		</Column>
 		<Column style={{ marginRight:"-100px", display: "flex", width: "300px", direction:"rtl" }} >
-			<Heading style={{ marginLeft:"100px" }}>تابعونا</Heading>
+			<Heading style={{ marginLeft:"100px", direction:"rtl" }}>تابعونا</Heading>
                 <p style={{ direction: "rtl" }}>
 				يمكنكم أيضا التواصل معنا عبر وسائل وسائل التواصل الاجتماعي ومشاركتنا تطلعاتكم  وأفكاركم وتعليقاتكم التي تُسعدنا، مع تحيات مجلة الشهاب العلميّ
 				</p>
 			<div style={{ 
-				direction:"ltr", 
+				direction:"rtl", 
 				display: "inline", 
 				marginLeft: "auto", 
 				marginRight: "auto",
-				marginTop: "30px",
-				width: "200px"
+				marginTop: "10px",
+				width: "100px"
 			}}>
 				<FooterLink style={{ width: "80px", height: "80px" }} className='Social' href="https://www.facebook.com/SiriusMagazine">
 					<FacebookIcon sx={{ width: 40, height: 40 }} />
@@ -59,12 +61,12 @@ return (
 				<FooterLink className='Social' href="https://www.instagram.com/sirius.algeria/">
 					<InstagramIcon sx={{ width: 40, height: 40 }} />
 				</FooterLink>
-				<FooterLink className='Social' href="#">
+				{/* <FooterLink className='Social' href="#">
 					<YouTubeIcon sx={{ width: 40, height: 40 }} />
-				</FooterLink>
-				<FooterLink className='Social' href="#">
+				</FooterLink> */}
+				{/* <FooterLink className='Social' href="#">
 					<TwitterIcon sx={{ width: 40, height: 40 }} />
-				</FooterLink>
+				</FooterLink> */}
 			</div>
 			{/* <FooterLink href="#">Coding</FooterLink>
 			<FooterLink href="#">Teaching</FooterLink> */}
@@ -72,37 +74,43 @@ return (
 		<Column>
 			<Heading>عنوان المجلة</Heading>
 			<FooterLink href="#">
-				<p style={{ direction:"rtl", width:"250px", marginRight:"-60px", fontSize:"15px" }} >
+				<p style={{ direction:"rtl", width:"250px", fontSize:"15px" }} >
 					وحدة البحث في الوساطة العلمية -CERIST
 					جامعة قسطينة 3	
 				</p>
-				<p style={{ direction:"rtl", width:"250px", marginRight: "-60px" }}>
+				<p style={{ direction:"rtl", width:"250px" }}>
 					contact@siriusalgeria-mag.net
 					Phone : +213 (0)771 56 06 58
 				</p>
 			</FooterLink>
-			<FooterLink href="https://www.paypal.com/donate?token=-_DbW3TGtjrET5eTRpFdbz-lPvjDPerlg_Kc9aA4AtpVy52J0xxO6Btf08foUqsot5yqJz-4zVfQi0de">
+			{/* <FooterLink href="https://www.paypal.com/donate?token=-_DbW3TGtjrET5eTRpFdbz-lPvjDPerlg_Kc9aA4AtpVy52J0xxO6Btf08foUqsot5yqJz-4zVfQi0de">
 				<img 
 					src={process.env.PUBLIC_URL + `/donation.png`}
 					alt=""
 					onClick="https://www.paypal.com/donate?token=-_DbW3TGtjrET5eTRpFdbz-lPvjDPerlg_Kc9aA4AtpVy52J0xxO6Btf08foUqsot5yqJz-4zVfQi0de"
 				/>
-			</FooterLink>
+			</FooterLink> */}
 		</Column>
 		<Column style={{ width:"310px" }} >
-			<Heading>إشترك معنا ليصلك كل جديد.</Heading>
+			{/* <Heading>إشترك معنا ليصلك كل جديد.</Heading> */}
 			<div style={{ 
 				width:"300px", 
 				display: "inline-block", 
-				border: "1px solid", 
-				marginBottom: "20px",
-				borderRadius: "5px" 
+				marginTop: "80px",
 			}} >
-				<CustomInput 
+				<FooterLink href="https://www.paypal.com/donate?token=-_DbW3TGtjrET5eTRpFdbz-lPvjDPerlg_Kc9aA4AtpVy52J0xxO6Btf08foUqsot5yqJz-4zVfQi0de">
+					<img 
+						style={{ marginBottom: "20px" }}
+						src={process.env.PUBLIC_URL + `/donation.png`}
+						alt=""
+						onClick="https://www.paypal.com/donate?token=-_DbW3TGtjrET5eTRpFdbz-lPvjDPerlg_Kc9aA4AtpVy52J0xxO6Btf08foUqsot5yqJz-4zVfQi0de"
+					/>
+				</FooterLink>
+				{/* <CustomInput 
 					aria-label="Demo input" 
 					placeholder=" Email..."
 				/>
-				<Button variant="contained">Submit</Button>
+				<Button variant="contained">Submit</Button> */}
 			</div>
 			<div style={{ display:"inline-block" }}>
 				<FooterLink href="#">
